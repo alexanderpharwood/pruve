35929713660d90904872f8aea612ccb51a573df6
alexanderpharwood <alex.harwood@live.co.uk>
2019-09-06 12:36:53 +0100

    New build and bundle and map

--- dist/bundle.js
+++ dist/bundle.js
@@ -281,10 +281,14 @@
   function (_TypeError) {
     _inherits(ValidationException, _TypeError);
 
-    function ValidationException() {
+    function ValidationException(errors) {
+      var _this;
+
       _classCallCheck(this, ValidationException);
 
-      return _possibleConstructorReturn(this, _getPrototypeOf(ValidationException).apply(this, arguments));
+      _this = _possibleConstructorReturn(this, _getPrototypeOf(ValidationException).call(this, 'Validation failed'));
+      _this.errors = errors;
+      return _this;
     }
 
     return ValidationException;
@@ -305,154 +309,239 @@
     };
   }
 
+  function addError(failing, key, error) {
+    if (validateArray(failing[key]) === false) {
+      failing[key] = [];
+    }
+
+    failing[key].push(error);
+    return failing[key];
+  }
+
+  function assessValueAgainstRuleset(value, key, ruleset) {
+    var failing = [];
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+      for (var _iterator = ruleset[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var rule = _step.value;
+        var ruleProps = getRuleProperties(rule);
+
+        switch (ruleProps.rule) {
+          case "string":
+            if (validateString(value) === false) {
+              failing[key] = addError(failing, key, key + ' must be a string');
+            }
+
+            break;
+
+          case "bool":
+            if (validateBool(value) === false) {
+              failing[key] = addError(failing, key, key + ' must be a boolean');
+            }
+
+            break;
+
+          case "number":
+            if (validateNumber(value) === false) {
+              failing[key] = addError(failing, key, key + ' is not a number');
+            }
+
+            break;
+
+          case "int":
+            if (validateInt(value) === false) {
+              failing[key] = addError(failing, key, key + ' must be an integer');
+            }
+
+            break;
+
+          case "float":
+            if (validateFloat(value) === false) {
+              failing[key] = addError(failing, key, key + ' must be a float');
+            }
+
+            break;
+
+          case "array":
+            if (validateArray(value) === false) {
+              failing[key] = addError(failing, key, key + ' must be an array');
+            }
+
+            break;
+
+          case "object":
+            if (validateObject(value) === false) {
+              failing[key] = addError(failing, key, key + ' must be an object');
+            }
+
+            break;
+
+          case "date":
+            if (validateDate(value) === false) {
+              failing[key] = addError(failing, key, key + ' must be a date');
+            }
+
+            break;
+
+          case "null":
+            if (validateNull(value) === false) {
+              failing[key] = addError(failing, key, key + ' must be null');
+            }
+
+            break;
+
+          case "undefined":
+            if (validateUndefined(value) === false) {
+              failing[key] = addError(failing, key, key + ' must be undefined');
+            }
+
+            break;
+
+          case "function":
+            if (validateFunction(value) === false) {
+              failing[key] = addError(failing, key, key + ' must be a function');
+            }
+
+            break;
+
+          case "max":
+            if (validateMax(value, parseInt(ruleProps.condition)) === false) {
+              failing[key] = addError(failing, key, key + ' must be less than ' + ruleProps.condition + ' in length or value');
+            }
+
+            break;
+
+          case "min":
+            if (validateMin(value, parseInt(ruleProps.condition)) === false) {
+              failing[key] = addError(failing, key, key + ' must be greater than ' + ruleProps.condition + ' in length or value');
+            }
+
+            break;
+
+          case "defined":
+            if (validateDefined(value) === false) {
+              failing[key] = addError(failing, key, key + ' must be defined');
+            }
+
+            break;
+
+          case "email":
+            if (validateEmail(value) === false) {
+              failing[key] = addError(failing, key, key + ' must be a valid email address');
+            }
+
+            break;
+
+          case "has":
+            if (validateHas(value, ruleProps.condition) === false) {
+              failing[key] = addError(failing, key, key + ' must have property: ' + ruleProps.condition);
+            }
+
+            break;
+        }
+      }
+    } catch (err) {
+      _didIteratorError = true;
+      _iteratorError = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion && _iterator["return"] != null) {
+          _iterator["return"]();
+        }
+      } finally {
+        if (_didIteratorError) {
+          throw _iteratorError;
+        }
+      }
+    }
+
+    return failing;
+  }
+
   function validatePasses (values, rules) {
-    if (validateObject(values) === false) {
-      throw new TypeError('Value must be an object, with propeties to validate.');
+    if (validateObject(values) === false && validateArray(values) === false) {
+      throw new TypeError('Value must be an object or array, with propeties to validate.');
     }
 
     if (validateObject(rules) === false) {
       throw new TypeError('Rules must be an object, with propeties to validate.');
     }
 
-    var failing = [];
+    var failing = {};
 
     for (var prop in rules) {
-      var rulesList = rules[prop].split('.');
-      var _iteratorNormalCompletion = true;
-      var _didIteratorError = false;
-      var _iteratorError = undefined;
+      var ruleset = rules[prop].split('.');
 
-      try {
-        for (var _iterator = rulesList[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-          var rule = _step.value;
-          var ruleProps = getRuleProperties(rule);
+      if (prop === '*') {
+        for (var key in values) {
+          var result = assessValueAgainstRuleset(values[key], key, ruleset);
 
-          switch (ruleProps.rule) {
-            case "string":
-              if (validateString(values[prop]) === false) {
-                failing.push(prop + ' must be a string');
+          if (Object.keys(result).length > 0) {
+            if (typeof failing[key] !== 'undefined') {
+              var _iteratorNormalCompletion2 = true;
+              var _didIteratorError2 = false;
+              var _iteratorError2 = undefined;
+
+              try {
+                for (var _iterator2 = result[key][Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+                  var failure = _step2.value;
+                  failing[key].push(failure);
+                }
+              } catch (err) {
+                _didIteratorError2 = true;
+                _iteratorError2 = err;
+              } finally {
+                try {
+                  if (!_iteratorNormalCompletion2 && _iterator2["return"] != null) {
+                    _iterator2["return"]();
+                  }
+                } finally {
+                  if (_didIteratorError2) {
+                    throw _iteratorError2;
+                  }
+                }
               }
-
-              break;
-
-            case "bool":
-              if (validateBool(values[prop]) === false) {
-                failing.push(prop + ' must be a boolean');
-              }
-
-              break;
-
-            case "number":
-              if (validateNumber(values[prop]) === false) {
-                failing.push(prop + ' is not a number');
-              }
-
-              break;
-
-            case "int":
-              if (validateInt(values[prop]) === false) {
-                failing.push(prop + ' must be an integer');
-              }
-
-              break;
-
-            case "float":
-              if (validateFloat(values[prop]) === false) {
-                failing.push(prop + ' must be a float');
-              }
-
-              break;
-
-            case "array":
-              if (validateArray(values[prop]) === false) {
-                failing.push(prop + ' must be an array');
-              }
-
-              break;
-
-            case "object":
-              if (validateObject(values[prop]) === false) {
-                failing.push(prop + ' must be an object');
-              }
-
-              break;
-
-            case "date":
-              if (validateDate(values[prop]) === false) {
-                failing.push(prop + ' must be a date');
-              }
-
-              break;
-
-            case "null":
-              if (validateNull(values[prop]) === false) {
-                failing.push(prop + ' must be null');
-              }
-
-              break;
-
-            case "undefined":
-              if (validateUndefined(values[prop]) === false) {
-                failing.push(prop + ' must be undefined');
-              }
-
-              break;
-
-            case "function":
-              if (validateFunction(values[prop]) === false) {
-                failing.push(prop + ' must be a function');
-              }
-
-              break;
-
-            case "max":
-              if (validateMax(values[prop], parseInt(ruleProps.condition)) === false) {
-                failing.push(prop + ' must be less than ' + ruleProps.condition);
-              }
-
-              break;
-
-            case "min":
-              if (validateMin(values[prop], parseInt(ruleProps.condition)) === false) {
-                failing.push(prop + ' must be greater than ' + ruleProps.condition);
-              }
-
-              break;
-
-            case "defined":
-              if (validateDefined(values[prop]) === false) {
-                failing.push(prop + ' must be defined');
-              }
-
-              break;
-
-            case "email":
-              if (validateEmail(values[prop]) === false) {
-                failing.push(prop + ' must be a valid email address');
-              }
-
-              break;
-
-            case "has":
-              if (validateHas(values[prop], ruleProps.condition) === false) {
-                failing.push(prop + ' must have property: ' + ruleProps.condition);
-              }
-
-              break;
+            } else {
+              Object.assign(failing, result);
+            }
           }
         }
-      } catch (err) {
-        _didIteratorError = true;
-        _iteratorError = err;
-      } finally {
-        try {
-          if (!_iteratorNormalCompletion && _iterator["return"] != null) {
-            _iterator["return"]();
+
+        continue;
+      }
+
+      var result = assessValueAgainstRuleset(values[prop], prop, ruleset);
+
+      if (Object.keys(result).length > 0) {
+        if (typeof failing[prop] !== 'undefined') {
+          var _iteratorNormalCompletion3 = true;
+          var _didIteratorError3 = false;
+          var _iteratorError3 = undefined;
+
+          try {
+            for (var _iterator3 = result[prop][Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
+              var _failure = _step3.value;
+              failing[prop].push(_failure);
+            }
+          } catch (err) {
+            _didIteratorError3 = true;
+            _iteratorError3 = err;
+          } finally {
+            try {
+              if (!_iteratorNormalCompletion3 && _iterator3["return"] != null) {
+                _iterator3["return"]();
+              }
+            } finally {
+              if (_didIteratorError3) {
+                throw _iteratorError3;
+              }
+            }
           }
-        } finally {
-          if (_didIteratorError) {
-            throw _iteratorError;
-          }
+        } else {
+          Object.assign(failing, result);
         }
       }
     }
@@ -482,9 +571,11 @@
       value: function passes(rules) {
         var failing = validatePasses(this.value, rules);
 
-        if (failing.length > 0) {
+        if (Object.keys(failing).length > 0) {
           throw new ValidationException(failing);
         }
+
+        return this;
       }
     }, {
       key: "string",
