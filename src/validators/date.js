1b9777506ac35d72f176c2a582e28175fb82d859
alexanderpharwood <alex.h@vms-group.co.uk>
2021-11-08 16:07:06 +0000

    Fix date validation

--- src/validators/date.js
+++ src/validators/date.js
@@ -1,6 +1,7 @@
-export default function(value){
-	if (value instanceof Date) return true;
-	const dateInstance = new Date(value);
-	if (dateInstance.toString() === "Invalid Date") return false;
-	return isNaN(dateInstance.getTime()) === false;
+export default function (value) {
+  if (value instanceof Date) return true;
+  if (value === null) return false;
+  const dateInstance = new Date(value);
+  if (dateInstance.toString() === "Invalid Date") return false;
+  return isNaN(dateInstance.getTime()) === false;
 }
